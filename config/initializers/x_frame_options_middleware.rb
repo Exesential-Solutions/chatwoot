f4a61bcfb71216bafd9fc3c57773db6f0697619a
chunf <chun_foong@hotmail.com>
2023-12-28 17:30:22 +0800

    add xframe

---
+++ config/initializers/x_frame_options_middleware.rb
@@ -0,0 +1,14 @@
+Rails.application.config.middleware.insert_before 0,  ->(app) {
+    class Middleware
+      def initialize(app)
+        @app = app
+      end
+
+      def call(env)
+        status, headers, response = @app.call(env)
+        headers['X-Frame-Options'] = 'ALLOW-FROM https://localhost:8084'
+        [status, headers, response]
+      end
+    end
+    Middleware
+}
